99d0697a6f5c9144f7ad43846d899d9a70254d31
Robin Wagner <robin.wagner@axa.de>
2024-10-23 09:49:34 +0200

    feature: added option to recalculate all prices

--- src/main/java/de/axa/robin/vertragsverwaltung/modell/Vertrag.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/modell/Vertrag.java
@@ -46,7 +46,7 @@ public class Vertrag {
             if(!monatlich){
                 preis = (alter * 0.1 + fahrzeug.getHoechstgeschwindigkeit()  * 0.4)*24;
             } else{
-                preis = (alter * 0.1 + fahrzeug.getHoechstgeschwindigkeit() * 0.4)*2;
+                preis = (alter * 0.1 + fahrzeug.getHoechstgeschwindigkeit() * 0.4)*1.5;
             }
         } catch (Exception e) {
             Output.invalidinput();
