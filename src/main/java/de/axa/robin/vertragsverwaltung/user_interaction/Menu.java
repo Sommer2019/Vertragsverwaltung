efa36495107249a5c6e014283175915e5beb80db
Robin Wagner <robin.wagner@axa.de>
2024-10-28 13:01:02 +0100

    internal: better overview

--- src/main/java/de/axa/robin/vertragsverwaltung/user_interaction/Menu.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/user_interaction/Menu.java
@@ -1,22 +1,27 @@
 package de.axa.robin.vertragsverwaltung.user_interaction;
 
+import de.axa.robin.vertragsverwaltung.modell.Vertrag;
 import de.axa.robin.vertragsverwaltung.storage.Vertragsverwaltung;
 import de.axa.robin.vertragsverwaltung.storage.editor.Create;
 import de.axa.robin.vertragsverwaltung.storage.editor.Delete;
 import de.axa.robin.vertragsverwaltung.storage.editor.Edit;
 import de.axa.robin.vertragsverwaltung.user_interaction.Input.Allgemein;
 
+import java.util.List;
+
 public class Menu {
     public static void menu() {
         while (true) {
             int number = Allgemein.menu();
+            List vertrage = Vertragsverwaltung.getVertrage();
             if (number == 1) {
-                Output.druckeVertrage(Vertragsverwaltung.getVertrage());
+                Output.druckeVertrage(vertrage);
             }
             if (number == 2) {
                 int vsnr = Allgemein.getvsnr();
+                Vertrag vertrag = Vertragsverwaltung.getVertrag(vsnr);
                 if(vsnr!=0){
-                    Output.druckeVertrag(Vertragsverwaltung.getVertrag(vsnr));
+                    Output.druckeVertrag(vertrag);
                 }
             }
             if (number == 3) {
