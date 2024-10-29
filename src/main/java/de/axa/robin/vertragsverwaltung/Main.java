49e5db8e27d8aa4da16f790b7ac4caed1d7b1d09
Robin Wagner <robin.wagner@axa.de>
2024-10-29 08:06:21 +0100

    internal: removed unnecessary statics

--- src/main/java/de/axa/robin/vertragsverwaltung/Main.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/Main.java
@@ -3,7 +3,7 @@ package de.axa.robin.vertragsverwaltung;
 import de.axa.robin.vertragsverwaltung.user_interaction.Menu;
 
 class Main {
-    public void main(String[] args) {
+    void main() {
         Menu menu = new Menu();
         menu.menu();
     }
