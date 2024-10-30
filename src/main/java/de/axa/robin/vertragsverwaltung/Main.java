7ad274647a2c32c6d16c3414b18ef58826266ba6
Robin Wagner <robin.wagner@axa.de>
2024-10-30 08:49:13 +0100

    internal: updated dependencies

--- src/main/java/de/axa/robin/vertragsverwaltung/Main.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/Main.java
@@ -3,7 +3,7 @@ package de.axa.robin.vertragsverwaltung;
 import de.axa.robin.vertragsverwaltung.user_interaction.Menu;
 
 class Main {
-    void main() {
+    public static void main(String[] args) {
         Menu menu = new Menu();
         menu.menu();
     }
