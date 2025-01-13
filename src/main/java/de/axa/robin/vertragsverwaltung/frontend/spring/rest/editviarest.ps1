1cd5a9f37989d0ebc36e0e2e760264c5a6ee5e69
Robin Wagner <robin.wagner@axa.de>
2025-01-13 08:55:52 +0100

    feature: rest-controller setup instructions ps

--- src/main/java/de/axa/robin/vertragsverwaltung/frontend/spring/rest/editviarest.ps1
+++ src/main/java/de/axa/robin/vertragsverwaltung/frontend/spring/rest/editviarest.ps1
@@ -1,3 +1,11 @@
+$uri = "http://localhost:8080/api/vertrage/10000000"
 $body = '{"partner": {"geburtsdatum": "1956-06-12"}}'
 
 Invoke-WebRequest -Uri $uri -Method PUT -Headers $headers -Body $body -WebSession $session
+
+$uri = "http://localhost:8080/api/vertrage"
+Invoke-WebRequest -Uri $uri -Method GET -Headers $headers -WebSession $session
+
+Invoke-WebRequest -Uri $uri -Method GET -Headers $headers -WebSession $session
+
+Invoke-WebRequest -Uri $uri -Method DELETE -Headers $headers -WebSession $session
--- src/main/java/de/axa/robin/vertragsverwaltung/frontend/spring/rest/setuprestconnection.ps1
+++ src/main/java/de/axa/robin/vertragsverwaltung/frontend/spring/rest/setuprestconnection.ps1
@@ -8,7 +8,6 @@ $loginResponse = Invoke-WebRequest -Uri "http://localhost:8080/login" -Method PO
     _csrf = $csrfToken
 }
 
-$uri = "http://localhost:8080/api/vertrage/10000000"
 $headers = @{
     "Content-Type" = "application/json"
     "X-CSRF-TOKEN" = $csrfToken
