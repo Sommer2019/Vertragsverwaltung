02a952cc5e7c234b97673307602d011d71a91c36
Robin Wagner <robin.wagner@axa.de>
2025-01-22 14:31:27 +0100

    feat: fixed invalid axa-syntax in api

--- src/main/java/de/axa/robin/vertragsverwaltung/api/VertragController.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/api/VertragController.java
@@ -2,6 +2,8 @@ package de.axa.robin.vertragsverwaltung.api;
 
 import de.axa.robin.vertragsverwaltung.backend.modell.Vertrag;
 import de.axa.robin.vertragsverwaltung.backend.storage.Vertragsverwaltung;
+import de.axa.robin.vertragsverwaltung.backend.storage.editor.Create;
+import de.axa.robin.vertragsverwaltung.backend.storage.editor.Edit;
 import de.axa.robin.vertragsverwaltung.backend.storage.validators.InputValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +16,14 @@ import java.util.Optional;
 public class VertragController {
     private final InputValidator inputValidator;
     private final Vertragsverwaltung Vertragsverwaltung;
+    private final Edit edit;
+    private final Create create;
 
     public VertragController(InputValidator inputValidator, Vertragsverwaltung Vertragsverwaltung) {
         this.inputValidator = inputValidator;
         this.Vertragsverwaltung = Vertragsverwaltung;
+        this.edit = new Edit(Vertragsverwaltung);
+        this.create = new Create(Vertragsverwaltung);
     }
 
     @GetMapping
@@ -26,8 +32,9 @@ public class VertragController {
         return ResponseEntity.ok(vertrage);
     }
 
-    @PostMapping
+    @PutMapping
     public ResponseEntity<Vertrag> createVertrag(@RequestBody Vertrag vertrag) {
+        vertrag.setPreis(create.createPreis(vertrag.isMonatlich(), vertrag.getPartner(), vertrag.getFahrzeug()));
         if (inputValidator.isInvalidVertrag(vertrag)) {
             return ResponseEntity.status(400).build();
         }
@@ -42,11 +49,10 @@ public class VertragController {
                 .orElseGet(() -> ResponseEntity.status(404).build());
     }
 
-    @PutMapping("/{id}")
+    @PostMapping("/{id}")
     public ResponseEntity<Vertrag> updateVertrag(@PathVariable Integer id, @RequestBody Vertrag vertrag) {
-        if (inputValidator.isInvalidVertrag(vertrag)) {
-            return ResponseEntity.status(400).build();
-        }
+        vertrag = edit.updateVertragFields(vertrag, Vertragsverwaltung.getVertrag(id));
+        vertrag.setPreis(create.createPreis(vertrag.isMonatlich(), vertrag.getPartner(), vertrag.getFahrzeug()));
         boolean deleted = Vertragsverwaltung.vertragLoeschen(id);
         if (!deleted) {
             return ResponseEntity.status(404).build();
--- src/main/java/de/axa/robin/vertragsverwaltung/backend/storage/editor/Edit.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/backend/storage/editor/Edit.java
@@ -1,6 +1,8 @@
 package de.axa.robin.vertragsverwaltung.backend.storage.editor;
 
 import de.axa.robin.vertragsverwaltung.backend.config.Setup;
+import de.axa.robin.vertragsverwaltung.backend.modell.Fahrzeug;
+import de.axa.robin.vertragsverwaltung.backend.modell.Partner;
 import de.axa.robin.vertragsverwaltung.backend.modell.Vertrag;
 import de.axa.robin.vertragsverwaltung.backend.storage.Repository;
 import de.axa.robin.vertragsverwaltung.backend.storage.Vertragsverwaltung;
@@ -37,5 +39,73 @@ public class Edit {
         }
         System.out.println("Neue Summe aller Beiträge im Jahr: " + summe.setScale(2, RoundingMode.HALF_DOWN) + "€");
         return summe;
+    }
+    public Vertrag updateVertragFields(Vertrag source, Vertrag target) {
+        if (source.getVsnr() != 0) {
+            target.setVsnr(source.getVsnr());
+        }
+        if (source.getVersicherungsbeginn() != null && !source.getVersicherungsbeginn().toString().equals("nullnull")) {
+            target.setVersicherungsbeginn(source.getVersicherungsbeginn());
+        }
+        if (source.getVersicherungsablauf() != null && !source.getVersicherungsablauf().toString().equals("nullnull")) {
+            target.setVersicherungsablauf(source.getVersicherungsablauf());
+        }
+        if (source.getAntragsDatum() != null && !source.getAntragsDatum().toString().equals("nullnull")) {
+            target.setAntragsDatum(source.getAntragsDatum());
+        }
+        if (source.getPartner() != null) {
+            Partner sourcePartner = source.getPartner();
+            Partner targetPartner = target.getPartner();
+            if (sourcePartner.getVorname() != null && !sourcePartner.getVorname().isEmpty()) {
+                targetPartner.setVorname(sourcePartner.getVorname());
+            }
+            if (sourcePartner.getNachname() != null && !sourcePartner.getNachname().isEmpty()) {
+                targetPartner.setNachname(sourcePartner.getNachname());
+            }
+            if (sourcePartner.getGeschlecht()!=' ') {
+                targetPartner.setGeschlecht(sourcePartner.getGeschlecht());
+            }
+            if (sourcePartner.getGeburtsdatum() != null && !sourcePartner.getGeburtsdatum().toString().equals("nullnull")) {
+                targetPartner.setGeburtsdatum(sourcePartner.getGeburtsdatum());
+            }
+            if (sourcePartner.getLand() != null && !sourcePartner.getLand().isEmpty()) {
+                targetPartner.setLand(sourcePartner.getLand());
+            }
+            if (sourcePartner.getStrasse() != null && !sourcePartner.getStrasse().isEmpty()) {
+                targetPartner.setStrasse(sourcePartner.getStrasse());
+            }
+            if (sourcePartner.getHausnummer() != null && !sourcePartner.getHausnummer().isEmpty()) {
+                targetPartner.setHausnummer(sourcePartner.getHausnummer());
+            }
+            if (sourcePartner.getPlz() != null && !sourcePartner.getPlz().isEmpty()) {
+                targetPartner.setPlz(sourcePartner.getPlz());
+            }
+            if (sourcePartner.getStadt() != null && !sourcePartner.getStadt().isEmpty()) {
+                targetPartner.setStadt(sourcePartner.getStadt());
+            }
+            if (sourcePartner.getBundesland() != null && !sourcePartner.getBundesland().isEmpty()) {
+                targetPartner.setBundesland(sourcePartner.getBundesland());
+            }
+        }
+        if (source.getFahrzeug() != null) {
+            Fahrzeug sourceFahrzeug = source.getFahrzeug();
+            Fahrzeug targetFahrzeug = target.getFahrzeug();
+            if (sourceFahrzeug.getAmtlichesKennzeichen() != null && !sourceFahrzeug.getAmtlichesKennzeichen().isEmpty()) {
+                targetFahrzeug.setAmtlichesKennzeichen(sourceFahrzeug.getAmtlichesKennzeichen());
+            }
+            if (sourceFahrzeug.getHersteller() != null && !sourceFahrzeug.getHersteller().isEmpty()) {
+                targetFahrzeug.setHersteller(sourceFahrzeug.getHersteller());
+            }
+            if (sourceFahrzeug.getTyp() != null && !sourceFahrzeug.getTyp().isEmpty()) {
+                targetFahrzeug.setTyp(sourceFahrzeug.getTyp());
+            }
+            if (sourceFahrzeug.getHoechstgeschwindigkeit() != 0) {
+                targetFahrzeug.setHoechstgeschwindigkeit(sourceFahrzeug.getHoechstgeschwindigkeit());
+            }
+            if (sourceFahrzeug.getWagnisskennziffer() != 0) {
+                targetFahrzeug.setWagnisskennziffer(sourceFahrzeug.getWagnisskennziffer());
+            }
+        }
+        return target;
     }
 }
