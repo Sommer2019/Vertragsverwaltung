5368935dfd7de4674110677f99886063995a782a
Robin Wagner <robin.wagner@axa.de>
2025-02-06 13:17:20 +0100

    chore: added next todo steps

--- src/main/java/de/axa/robin/vertragsverwaltung/VertragsverwaltungApplication.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/VertragsverwaltungApplication.java
@@ -3,7 +3,7 @@ package de.axa.robin.vertragsverwaltung;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "de.axa.robin.vertragsverwaltung") //ToDO: recreate tests
+@SpringBootApplication(scanBasePackages = "de.axa.robin.vertragsverwaltung") //ToDO: recreate tests for frontend.storage
 //ToDo: Kommentare
 public class VertragsverwaltungApplication {
     public static void main(String[] args) {
