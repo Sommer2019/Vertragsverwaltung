c122ce8fb81f970b46d1463d41676804020eaf49
Robin Wagner <robin.wagner@axa.de>
2025-02-07 06:39:14 +0100

    chore: cleaned up pom

--- src/main/java/de/axa/robin/vertragsverwaltung/VertragsverwaltungApplication.java
+++ src/main/java/de/axa/robin/vertragsverwaltung/VertragsverwaltungApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "de.axa.robin.vertragsverwaltung")
 //ToDo: Kommentare
-//ToDO: POM aufräumen
 public class VertragsverwaltungApplication {
     public static void main(String[] args) {
         SpringApplication.run(VertragsverwaltungApplication.class, args);
